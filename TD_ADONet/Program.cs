09c3842896a21a42be2c7e95bda48b73f743a207
Kerebel <bastienkerebel@gmail.com>
2021-05-03 13:49:30 +0200

    Base

--- TD_ADONet/Program.cs
+++ TD_ADONet/Program.cs
@@ -1,4 +1,5 @@
-﻿using EmployeDatas.Oracle;
+﻿using EmployeDatas.Mysql;
+using EmployeDatas.Oracle;
 using MySql.Data.MySqlClient;
 using Oracle.ManagedDataAccess.Client;
 using System;
@@ -20,14 +21,14 @@ namespace TD_ADONet
                 EmployeOracle empOracle = new EmployeOracle(host, port, sid, login, pwd);
                 empOracle.Ouvrir();
                 empOracle.afficherTousLesCours();
-                //empOracle.afficherNbProjets();
-                //empOracle.AfficherSalaireMoyenParProjet();
-                //Console.WriteLine("Nombre de lignes curseur mises à jour : " + empOracle.AugmenterSalaireCurseur());
-                //empOracle.AfficherEmployesSalaire(10000);
-                //empOracle.AfficheSalaireEmploye(76);
-                //empOracle.InsereCours("BR099", "Apprentissage JDBC", 4);
-                //empOracle.supprimeCours("BR099");
-                //empOracle.AugmenterSalaire("PR2",2);
+                empOracle.afficherNbProjets();
+                empOracle.AfficherSalaireMoyenParProjet();
+                Console.WriteLine("Nombre de lignes curseur mises à jour : " + empOracle.AugmenterSalaireCurseur());
+                empOracle.AfficherEmployesSalaire(10000);
+                empOracle.AfficheSalaireEmploye(76);
+                empOracle.InsereCours("BR099", "Apprentissage JDBC", 4);
+                empOracle.supprimeCours("BR099");
+                empOracle.AugmenterSalaire("PR2", 2);
                 empOracle.Fermer();
                 /*
                 * 
@@ -50,21 +51,21 @@ namespace TD_ADONet
             String pwdMysql = "employeado";
             try
             {
-                // EmployeMysql empMysql = new EmployeMysql(hostMysql, portMysql, baseMysql, loginMysql, pwdMysql);
-                //empMysql.Ouvrir();
-                // Console.WriteLine("connecté Mysql");
+                EmployeMysql empMysql = new EmployeMysql(hostMysql, portMysql, baseMysql, loginMysql, pwdMysql);
+                empMysql.Ouvrir();
+                Console.WriteLine("connecté Mysql");
 
-                // empMysql.AfficherTousLesEmployes();
-                // empMysql.AfficherNbSeminaires();
-                // empMysql.AfficherNbInscritsParCours();
-                // Console.WriteLine("Nombre de lignes curseur mises à jour : " + empMysql.AugmenterSalaireCurseur());
-                // empMysql.AfficheProjetsNbEmployes(3);
-                // empMysql.SeminairesPosterieurs(new DateTime(2019,12,15));
-                // empMysql.InsereProjet("PR9","Alerte à Malibu", new DateTime(2019, 11,20), new DateTime(2020,6,30), "Monsieur Jean Tienun");
-                // empMysql.SupprimeSeminaire("BR0340413"); 
-                // empMysql.AjouterNbJoursCours(1, 3);
-                // empMysql.Fermer();
-                // Console.WriteLine("déconnecté Mysql");
+                empMysql.AfficherTousLesEmployes();
+                empMysql.AfficherNbSeminaires();
+                empMysql.AfficherNbInscritsParCours();
+                Console.WriteLine("Nombre de lignes curseur mises à jour : " + empMysql.AugmenterSalaireCurseur());
+                empMysql.AfficheProjetsNbEmployes(3);
+                empMysql.SeminairesPosterieurs(new DateTime(2019, 12, 15));
+                empMysql.InsereProjet("PR9", "Alerte à Malibu", new DateTime(2019, 11, 20), new DateTime(2020, 6, 30), "Monsieur Jean Tienun");
+                empMysql.SupprimeSeminaire("BR0340413");
+                empMysql.AjouterNbJoursCours(1, 3);
+                empMysql.Fermer();
+                Console.WriteLine("déconnecté Mysql");
 
             }
             catch (MySqlException ex)
